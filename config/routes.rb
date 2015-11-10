c09d3571f41d7bfdb7621ab8c7ec8bbebb3eb99b
benalun-jones <benalunjones@gmail.com>
2015-11-10 18:00:50 +0000

    modified app for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   root "static_pages#home"
   
   devise_for :users
-  resources :locations
-  resources :artists
-  resources :gigs
-  resources :users
+  resources :locations, :artists, :gigs, :users
 end
